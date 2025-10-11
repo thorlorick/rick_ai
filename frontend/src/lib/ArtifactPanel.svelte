fc8f9f259698d82b1a288d1f925367d401d2d890
ThorloRick <thorlorick@gmail.com>
2025-10-11 19:42:48 -0400

    Create ArtifactPanel.svelte

---
+++ frontend/src/lib/ArtifactPanel.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  import Prism from 'prismjs';
+  import 'prismjs/themes/prism-tomorrow.css';
+  
+  // Import common languages
+  import 'prismjs/components/prism-python';
+  import 'prismjs/components/prism-javascript';
+  import 'prismjs/components/prism-typescript';
+  import 'prismjs/components/prism-rust';
+  import 'prismjs/components/prism-go';
+  import 'prismjs/components/prism-java';
+  import 'prismjs/components/prism-cpp';
+  import 'prismjs/components/prism-c';
+  import 'prismjs/components/prism-css';
+  import
